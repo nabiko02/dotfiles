033444246192914865af3167738bd61e36d5b555
Nabiko02 <>
2025-12-31 13:21:23 +0100

    update hyprpaper script

--- hyprland/.config/hypr/change_wallpapers.sh
+++ hyprland/.config/hypr/change_wallpapers.sh
@@ -12,7 +12,8 @@ CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
-hyprctl hyprpaper reload ,"$WALLPAPER"
+echo $WALLPAPER
+hyprctl hyprpaper wallpaper ,"$WALLPAPER"
 
 HYPRLOCK_BACKGROUND="$HOME/.config/hypr/hyprlock-resources/hyprlock.png"
 ln -s -f $WALLPAPER $HYPRLOCK_BACKGROUND
